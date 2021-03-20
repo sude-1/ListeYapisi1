cdd28ab7b8210963370fe3daa01569c884aaccea
Sude Karanlık <sudekaranlikk@hotmail.com>
2021-03-20 19:46:57 +0300

    Add project files.

---
+++ ListeYapisi1/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListeYapisi1
+{
+    class MyDictionary<MyKey,MyValue>
+    {
+        (MyKey, MyValue)[] list;
+        public MyDictionary()
+        {
+            list = new (MyKey, MyValue)[0];
+        }
+
+        public void Add(MyKey CustomerNo,MyValue CustomerName)
+        {
+            (MyKey, MyValue)[] tempList = list;
+            list = new (MyKey, MyValue)[list.Length + 1];
+            for (int i = 0; i <tempList.Length; i++)
+            {
+                list[i] = tempList[i];
+            }
+
+            list[list.Length - 1] = (CustomerNo,CustomerName);
+        }
+
+        public int Count
+        {
+            get { return list.Length; }
+        }
+
+        public void Listing()
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine("Müşteri numarası ve Müşteri adı:" + list[i]);
+            }
+        }
+    }
+}
---
+++ ListeYapisi1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ListeYapisi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> customers = new MyDictionary<string, string>();
+            customers.Add("2314", "Sude Karanlık");
+            customers.Add("4352", "Mehmet Yıldız");
+            customers.Add("6754", "Hande Tatlı");
+
+            Console.WriteLine(customers.Count + " " + "Müşteri var");
+            customers.Listing();
+        }
+    }
+}
